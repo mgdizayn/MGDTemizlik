714ceb78785a5fc397fe7f9224eeac2153a2e797
mgdizayn <mgdizayn@hotmail.com>
2026-03-11 12:13:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const urlsToCache = [
     '/',
     '/index.html',
     'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js',
-    'https://unpkg.com/mqtt@5.0.3/dist/mqtt.min.js' // DÜZELTİLDİ: index.html ile aynı sürüm
+    'https://unpkg.com/mqtt@5.0.3/dist/mqtt.min.js'
 ];
 
 self.addEventListener('install', event => {
